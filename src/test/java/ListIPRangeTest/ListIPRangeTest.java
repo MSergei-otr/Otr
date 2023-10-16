c97ede9761f407bca9b757ead1ca5c85f2eb62e8
mikhailov.sergei <mikhailov.sergei@otr.ru>
2023-10-16 09:55:56 +0300

    add assertThat

--- src/test/java/ListIPRangeTest/ListIPRangeTest.java
+++ src/test/java/ListIPRangeTest/ListIPRangeTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static ListIPRange.ListIPRange.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ListIPRangeTest {
 
@@ -28,16 +30,13 @@ public class ListIPRangeTest {
         ListStrIP.add(3, "1.1");
         ListStrIP.add(4, "1.1.1");
         ListStrIP.add(5, "1.1.1.");
-        ListStrIP.add(6, "1.1.1.1");
 
         for (Object element : ListStrIP) {
             System.out.print(element + " ");
             boolean b = (isValidIP(element.toString()));
-            if (b) {
-                System.out.println("TrueIP = Passed");
-            } else {
-                System.out.println("TrueIP = Error");
-            }
+            assertThat (b, is(false));
         }
+        boolean b = (isValidIP("1.1.1.1"));
+        assertThat (b, is(true));
     }
 }
